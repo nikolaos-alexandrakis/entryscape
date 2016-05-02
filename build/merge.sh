a7abab02b8d4bceccd0edc370bd53b1b7aa22b32
Matthias Palmér <matthias@metasolutions.se>
2016-05-02 13:52:50 +0200

    Auto-create merged-nls and theme directories

--- build/merge.sh
+++ build/merge.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if ! [ -x "../merged-nls" ] ; then
+   mkdir ../merged-nls
+fi
+
+if ! [ -x "../theme" ] ; then
+   mkdir ../theme
+fi
+
 rm -rf ../merged-nls/*
 cp -a ../libs/entryscape-commons/nls/* ../merged-nls
 cp -a ../libs/entryscape-admin/nls/* ../merged-nls
