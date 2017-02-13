41508f1d86e1d86a221135e15c2cc02ad12f45bf
Matthias Palmér <matthias@metasolutions.se>
2017-02-13 11:08:58 +0100

    Switched to new grunt commands for building, installing etc

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,24 @@ module.exports = function(grunt) {
         nls: {
             langs: ["en", "sv", "da"],
             depRepositories: ["entryscape-commons", "entryscape-admin", "entryscape-catalog", "entryscape-terms", "entryscape-workbench"]
+        },
+        update: {
+            libs: [
+                'di18n',
+                'spa',
+                'rdfjson',
+                'rdforms',
+                'store',
+                'entryscape-commons',
+                'entryscape-admin',
+                'entryscape-catalog',
+                'entryscape-terms',
+                'entryscape-workbench'
+            ]
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-available-tasks');
+
 };
