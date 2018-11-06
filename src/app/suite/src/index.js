30a6a83a7d7f2db551f2a71e448d3a709b5ef8a6
Valentino Hudhra <v.hudhra@gmail.com>
2018-11-06 16:03:42 +0100

    Update suite config

--- src/app/suite/src/index.js
+++ src/app/suite/src/index.js
@@ -1,8 +1,8 @@
 import '@babel/polyfill';
-import 'commons/theme/privacy_en.html';
-import 'commons/theme/privacy_sv.html';
-import 'commons/theme/privacy_de.html';
-import 'mithril';
+// import '../assets/privacy_en.html';
+// import '../assets/privacy_sv.html';
+// import '../assets/privacy_de.html';
+
 import 'jquery';
 import 'popper.js';
 import 'bootstrap';
@@ -10,9 +10,11 @@ import 'bmd';
 import 'typeahead.js/dist/typeahead.jquery';
 import 'bloodhound-js'; // TODO @valentino typeahead alone seems to have issues, so import bloodhound seperately
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bmd/dist/css/bootstrap-material-design.css';
 import 'bmd/dist/css/ripples.css';
 import 'bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css';
+import 'font-awesome/css/font-awesome.min.css';
 import 'chartist/dist/chartist.min.css'; // TODO ?
 
 import initDefaults from './defaults';
