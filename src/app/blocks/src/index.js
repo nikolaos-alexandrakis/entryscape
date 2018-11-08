ba9bafd113e34b10ac76f68939e885a792c5eb91
scazan <scottcazan@gmail.com>
2018-11-08 10:49:30 +0100

    Adding basic entrypoints, updating .gitignore

---
+++ src/app/blocks/src/index.js
@@ -0,0 +1,26 @@
+import '@babel/polyfill';
+import 'mithril';
+import 'popper.js';
+import 'bmd';
+import 'typeahead.js/dist/typeahead.jquery';
+import 'bloodhound-js'; // TODO @valentino typeahead alone seems to have issues, so import bloodhound seperately
+
+// import 'bmd/dist/css/bootstrap-material-design.css';
+// import 'bmd/dist/css/ripples.css';
+// import 'bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css';
+// import 'chartist/dist/chartist.min.css'; // TODO ?
+
+
+import 'jquery';
+import 'jquery.mousewheel';
+import 'bootstrap';
+import 'bootstrap/modal';
+import 'bootstrap/popover';
+import 'bootstrap/button';
+import 'bootstrap/dropdown';
+import 'bootstrap/tooltip';
+import 'sizzle';
+
+import initDefaults from './defaults';
+
+initDefaults();
