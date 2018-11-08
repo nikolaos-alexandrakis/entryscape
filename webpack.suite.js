72a3fd8dd4879b4bac9f50bccf7be881c9aa845a
Valentino Hudhra <v.hudhra@gmail.com>
2018-11-08 12:13:58 +0100

    No need to have the file-loader in app specific webpack config

--- webpack.suite.js
+++ webpack.suite.js
@@ -53,16 +53,6 @@ module.exports = (env, argv) => {
             },
           ],
         },
-        {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          use: [{
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
-            },
-          }],
-        },
       ],
     },
     plugins: [
