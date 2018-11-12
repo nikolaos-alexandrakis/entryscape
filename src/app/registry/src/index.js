4d955736e5179a40ccb9f89d38052f38f970caac
Valentino Hudhra <v.hudhra@gmail.com>
2018-11-12 15:22:38 +0100

    Update registry dependencies

--- src/app/registry/src/index.js
+++ src/app/registry/src/index.js
@@ -1,11 +1,11 @@
 import '@babel/polyfill';
 import 'bloodhound-js'; // TODO @valentino typeahead alone seems to have issues, so import bloodhound seperately
-import 'bootstrap';
-import 'bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css';
-import 'bootstrap-material-design';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-material-design/dist/css/bootstrap-material-design.css';
 import 'bootstrap-material-design/dist/css/ripples.css';
+import 'bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css';
+import 'font-awesome/css/font-awesome.min.css';
 import 'chartist/dist/chartist.min.css';
 
 import 'jquery';
