8ad00160d8d4eb529e5f68db3a6629be73cd34c6
scazan <scottcazan@gmail.com>
2018-12-03 15:18:31 +0100

    Add flag-icon css to Registry

--- src/app/registry/src/index.js
+++ src/app/registry/src/index.js
@@ -6,17 +6,18 @@ import 'bootstrap';
 import 'bootstrap-material-design';
 import 'typeahead.js/dist/typeahead.jquery';
 import 'bloodhound-js'; // TODO @valentino typeahead alone seems to have issues, so import bloodhound seperately
+import registry from 'commons/registry';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-material-design/dist/css/bootstrap-material-design.css';
 import 'bootstrap-material-design/dist/css/ripples.css';
 import 'bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css';
 import 'font-awesome/css/font-awesome.min.css';
+import 'flag-icon-css/css/flag-icon.min.css';
 import 'chartist/dist/chartist.min.css'; // TODO ?
 import '../assets/privacy_de.html';
 import '../assets/privacy_en.html';
 import '../assets/privacy_sv.html';
-import registry from 'commons/registry';
 import initDefaults from './defaults';
 
 initDefaults(); // init defaults
