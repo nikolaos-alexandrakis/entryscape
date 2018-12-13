ad295427a6aa1d7c24120f73d5ea59ca276312c4
scazan <scottcazan@gmail.com>
2018-12-13 15:08:15 +0100

    Document createSetState

--- src/module/commons/src/util/util.js
+++ src/module/commons/src/util/util.js
@@ -4,6 +4,13 @@ import isUrl from 'is-url';
 
 export const isUri = stringToCheck => isUrl(stringToCheck);
 
+/**
+ * Returns a function for setting a closed "state" object
+ * @param {object} state
+ *
+ *
+ * @returns {function}
+ */
 export const createSetState = state => (props, avoidRedraw = false) => {
   Object.entries(props).forEach((keyVal) => {
     state[keyVal[0]] = keyVal[1];
