68f6d43770fe4631e9db633f8e8596b029422a34
scazan <scottcazan@gmail.com>
2018-12-18 12:26:01 +0100

    remove file

--- scripts/licenses/printLicenses.sh
+++ scripts/licenses/printLicenses.sh
@@ -1,41 +1,56 @@
 #!/usr/bin/env node
+const http = require('https');
 const _ = require('lodash');
-const fs = require('fs');
-const publicLicenseInformation = JSON.parse(fs.readFileSync('./scripts/licenses/licenses.json'));
 
-const stdin = process.openStdin();
+const request = http.get('https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json', (response) => {
 
-let data = '';
+  response.setEncoding('utf8');
+  let rawData = '';
+  response.on('data', (chunk) => { rawData += chunk; });
+  response.on('end', () => {
+    try {
+      const publicLicenseInformation = JSON.parse(rawData);
+      const stdin = process.openStdin();
 
-stdin.on('data', (chunk) => {
-  data += chunk;
-});
+      let data = '';
 
-stdin.on('end', () => {
-  const licenses = JSON.parse(data);
+      stdin.on('data', (chunk) => {
+        data += chunk;
+      });
 
-  Object.entries(licenses).forEach((keyVal) => {
-    const license = keyVal[1];
-    const name = keyVal[0];
-    const licenseID = license.licenses;
-    let licenseIDAndUrl = '';
-    if (typeof licenseID === 'string') {
-      const publicLicense = _.find(publicLicenseInformation.licenses, (publicLicenseInfo) => publicLicenseInfo.licenseId === licenseID.split('*').join(''));
+      stdin.on('end', () => {
+        const licenses = JSON.parse(data);
 
-      let licenseURL = '';
-      if (publicLicense) {
-        licenseURL = publicLicense.seeAlso.length > 0 ? publicLicense.seeAlso[0] : publicLicense.detailsUrl;
-      }
-      licenseIDAndUrl = `${licenseID}, ${licenseURL}`;
+        Object.entries(licenses).forEach((keyVal) => {
+          const license = keyVal[1];
+          const name = keyVal[0];
+          const licenseID = license.licenses;
+          let licenseIDAndUrl = '';
+          if (typeof licenseID === 'string') {
+            const publicLicense = _.find(publicLicenseInformation.licenses, (publicLicenseInfo) => publicLicenseInfo.licenseId === licenseID.split('*').join(''));
 
-    } else {
-      licenseIDAndUrl = `Unknown license`;
-    }
+            let licenseURL = '';
+            if (publicLicense) {
+              licenseURL = publicLicense.seeAlso.length > 0 ? publicLicense.seeAlso[0] : publicLicense.detailsUrl;
+            }
+            licenseIDAndUrl = `${licenseID}, ${licenseURL}`;
 
-    const licenseText = `${name}, ${license.repository}
+          } else {
+            licenseIDAndUrl = `Unknown license`;
+          }
+
+          const licenseText = `${name}, ${license.repository}
 License: ${licenseIDAndUrl}
 `;
 
-    console.log(licenseText);
+          console.log(licenseText);
+        });
+      });
+    } catch (e) {
+      console.error(e.message);
+    }
   });
 });
+
+
+
