5fdcbd29c487c7f7ff89223f11bbac3a93a5c709
Valentino Hudhra <v.hudhra@gmail.com>
2019-01-08 17:31:06 +0100

    Update Promise usage in generate api

--- src/module/catalog/src/datasets/GenerateAPI.js
+++ src/module/catalog/src/datasets/GenerateAPI.js
@@ -1,16 +1,16 @@
-import registry from 'commons/registry';
-import { promiseUtil } from 'store';
-import ProgressDialog from 'commons/progresstask/ProgressDialog';
-import TaskProgress from 'commons/progresstask/components/TaskProgress';
-import Row from 'commons/components/common/grid/Row';
 import Alert from 'commons/components/common/alert/Alert';
 import Button from 'commons/components/common/button/Button';
-import { clone, template } from 'lodash-es';
-import { i18n } from 'esi18n';
+import Row from 'commons/components/common/grid/Row';
+import TaskProgress from 'commons/progresstask/components/TaskProgress';
+import ProgressDialog from 'commons/progresstask/ProgressDialog';
+import registry from 'commons/registry';
 import config from 'config';
 import declare from 'dojo/_base/declare';
 import stamp from 'dojo/date/stamp';
+import { i18n } from 'esi18n';
+import { clone, merge, template } from 'lodash-es';
 import m from 'mithril';
+import { promiseUtil } from 'store';
 import api from './api';
 import pipelineUtil from './pipelineUtil';
 
@@ -60,7 +60,7 @@ export default declare([], {
       this.escaApiProgress[this.initialTasksState.init.nlsTaskName];
     this.initialTasksState.fileprocess.name =
       this.escaApiProgress[this.initialTasksState.fileprocess.nlsTaskName];
-    this.tasks = clone(this.initialTasksState);
+    this.tasks = this.initialTasksState;
     this.updateProgressDialog(this.tasks);
   },
   updateProgressDialog(tasks, updateFooter = false, errorMessage = null) {
@@ -110,42 +110,50 @@ export default declare([], {
       const rURIs = Object.keys(uri2FileDetails);
       const format = rURIs.every(rURI => uri2FileDetails[rURI].format === 'text/csv');
       if (!format) {
-        return dialogs.confirm(template(
-          this.escaFiles.onlyCSVSupported)({ format: format || '-' }),
-        this.escaFiles.confirmAPIActivation,
-        this.escaFiles.abortAPIActivation);
+        return dialogs.confirm(
+          template(
+            this.escaFiles.onlyCSVSupported)({ format: format || '-' }),
+          this.escaFiles.confirmAPIActivation,
+          this.escaFiles.abortAPIActivation);
       }
       return '';
     });
   },
-  getPipelineResource() {
-    return pipelineUtil.getPipelineResource().then((pres) => {
-      this.tasks.init.status = 'progress';
-      this.updateProgressDialog(this.tasks);
-      return pres;
-    });
+  updateProgressDialogState(state = {}, hasError = false, errorMessage = '') {
+    this.tasks = merge(this.tasks, state);
+    this.updateProgressDialog(this.tasks, hasError, errorMessage);
   },
   _processFiles(fileResourceURIs, pres) {
     const esu = registry.get('entrystoreutil');
     const async = registry.get('asynchandler');
     async.addIgnore('execute', true, true);
+    /**
+     *  For each of the files uris execute the pipeline sequentially
+     */
     return promiseUtil.forEach(fileResourceURIs, fileResourceURI =>
-      esu.getEntryByResourceURI(fileResourceURI).then(fEntry =>
-        pres.execute(fEntry, {}).then(result =>
-          this.checkApiStatus(result[0]).then(() => {
-            // this.updateUI(); file processed with message
-            this.noOfFiles += 1;
-            const apiFileProcessed = i18n.renderNLSTemplate(this.escaApiProgress.apiFileProcessed,
-              { number: this.noOfFiles, totalFiles: this.totalNoFiles });
-            this.tasks.fileprocess.message = apiFileProcessed;
-            this.updateProgressDialog(this.tasks);
-          }, (err) => {
-            // TODO Failure
-            this.tasks.fileprocess.status = 'failed';
-            this.updateProgressDialog(this.tasks, true,
-              this.escaApiProgress.apiProgressError); // change with nls
-            throw err;
-          }))));
+      esu.getEntryByResourceURI(fileResourceURI)
+        .then(fEntry => pres.execute(fEntry, {}))
+        .then(result => this.checkApiStatus(result[0]))
+        .then(() => {
+          // this.updateUI(); file processed with message
+          this.noOfFiles += 1;
+          const apiFileProcessed = i18n.renderNLSTemplate(this.escaApiProgress.apiFileProcessed,
+            { number: this.noOfFiles, totalFiles: this.totalNoFiles });
+          this.updateProgressDialogState({
+            fileprocess: {
+              message: apiFileProcessed,
+            },
+          });
+        })
+        .catch((err) => {
+          // TODO Failure
+          this.updateProgressDialogState({
+            fileprocess: {
+              status: 'failed',
+            },
+          }, true, this.escaApiProgress.apiProgressError); // change with nls
+          throw err;
+        }));
   },
   _createDistribution(pres) {
     this.distributionRow.createDistributionForAPI(pres).then(() => {
@@ -160,63 +168,116 @@ export default declare([], {
       this.datasetRow.listDistributions();
     });
   },
-  activateAPI() {
+  async activateAPI() {
     this.noOfCompletedTasks = 0;
     const async = registry.get('asynchandler');
     async.addIgnore('execute', true, true);
-    this.getPipelineResource().then((pres) => {
-      const es = registry.get('entrystore');
+    let tempFileURIs = clone(this.fileURIs);
+
+    try {
+      /**
+       * // TODO type
+       * @type store/Pipeline
+       */
+      const pipelineResource = await pipelineUtil.getPipelineResource();
+      this.updateProgressDialogState({ init: { status: 'progress' } });
+
+      /**
+       *
+       * @param resource
+       * @returns {*}
+       */
+      const transformId = pipelineResource.getTransformForType(pipelineResource.transformTypes.ROWSTORE);
+      pipelineResource.setTransformArguments(transformId, {});
+      pipelineResource.setTransformArguments(transformId, { action: 'create' });
+      await pipelineResource.commit();
+
+      /**
+       * @returns {entryPromise}
+       */
       const esu = registry.get('entrystoreutil');
-      let tempFileURIs = clone(this.fileURIs);
-      esu.getEntryByResourceURI(tempFileURIs[0]).then((fileEntry) => {
-        const transformId = pres.getTransformForType(pres.transformTypes.ROWSTORE);
-        pres.setTransformArguments(transformId, {});
-        pres.setTransformArguments(transformId, {
-          action: 'create',
-        });
-        pres.commit().then(() => {
-          this.tasks.init.status = 'done';
-          this.tasks.fileprocess.status = 'progress';
-          this.updateProgressDialog(this.tasks);
-          pres.execute(fileEntry, {}).then((result) => {
-            this.checkApiStatus(result[0]).then(() => {
-              tempFileURIs = tempFileURIs.slice(1); // remove first file entry
-              if (tempFileURIs.length === 0) {
-                this._createDistribution(result[0]);
-                return;
-              }
-              es.getEntry(result[0]).then((etlEntry) => { // start
-                pres.setTransformArguments(transformId, {});
-                pres.setTransformArguments(transformId, {
-                  action: 'append',
-                  datasetURL: etlEntry.getResourceURI(), // etlEntry pipelineResultEntryURI
-                });
-                return pres.commit().then(() => {
-                  this.noOfFiles += 1;
-                  const apiFileProcessed = i18n.renderNLSTemplate(this.escaApiProgress.apiFileProcessed,
-                    { number: this.noOfFiles, totalFiles: this.totalNoFiles });
-                  this.tasks.fileprocess.message = apiFileProcessed;
-                  this.updateProgressDialog(this.tasks);
-                  this._processFiles(tempFileURIs, pres).then(() => {
-                    // enable done button
-                    this._createDistribution(result[0]);
-                  });
-                });
-              });
-              //
-            }, () => {
-              // TODO Error code here
-              this.tasks.fileprocess.status = 'failed';
-              this.updateProgressDialog(this.tasks, true,
-                this.escaApiProgress.apiProgressError);
-            });
-          }, () => {
-            this.tasks.fileprocess.status = 'failed';
-            this.updateProgressDialog(this.tasks, true, this.escaApiProgress.apiProgressError);
-          });
-        });
+      const fileEntry = await esu.getEntryByResourceURI(tempFileURIs[0]);
+
+      /**
+       *
+       * @param entry
+       * @returns {*}
+       */
+      const pipelineResult = await pipelineResource.execute(fileEntry, {});
+      this.updateProgressDialogState({
+        init: { status: 'done' },
+        fileprocess: { status: 'progress' },
       });
-    });
+
+      /**
+       *
+       * @param result
+       * @returns {store|Entry|entryPromise|*|*}
+       */
+      tempFileURIs = tempFileURIs.slice(1); // remove first file entry
+      if (tempFileURIs.length === 0) {
+        this._createDistribution(pipelineResult[0]);
+      } else {
+        const pipelineResultEntry = await registry.get('entrystore').getEntry(pipelineResult[0]);
+
+        /**
+         *
+         * @param pipelineResultEntry
+         * @returns {Promise<any | never>}
+         */
+        pipelineResource.setTransformArguments(transformId, {});
+        pipelineResource.setTransformArguments(transformId, {
+          action: 'append',
+          datasetURL: pipelineResultEntry.getResourceURI(), // pipelineResultEntryURI
+        });
+        await pipelineResource.commit();
+
+        /**
+         * processFiles
+         * @returns {Promise<any | never>}
+         */
+        this.noOfFiles += 1;
+        this.updateProgressDialogState({
+          fileprocess: {
+            message: i18n.renderNLSTemplate(this.escaApiProgress.apiFileProcessed, {
+              number: this.noOfFiles,
+              totalFiles: this.totalNoFiles,
+            }),
+          },
+        });
+        await this._processFiles(tempFileURIs, pipelineResource);
+
+        /**
+         * createDistribution
+         */
+        this._createDistribution(pipelineResult[0]);
+      }
+    } catch (err) {
+      const message = this.escaApiProgress.apiProgressError;
+      this.updateProgressDialogState({ fileprocess: { status: 'failed' } }, true, message);
+    }
+    /**
+     * @param err
+     */
+    // const handleErrors = (err) => {
+    //   if (err !== 'file') { // TODO remove
+    //     const message = this.NLSLocalized.escaApiProgress.apiProgressError;
+    //     this.updateProgressDialogState({ fileprocess: { status: 'failed' } }, true, message);
+    //   }
+    // };
+
+    /**
+     * Activate API process
+     */
+    //pipelineResourcePromise
+    //  .then(updateTransformType)
+    // .then(getEntryByResourceURI)
+    //.then(executePipeline)
+    //.then(getFileEntry)
+    //.then(updateTransformArgs)
+    //.then(processFiles)
+    //.then(createDistribution)
+    //.catch(handleErrors);
   },
   updateApiDistribution() {
     const distMetadata = this.apiDistEntry.getMetadata();
@@ -323,15 +384,15 @@ export default declare([], {
   _getApiStatus(etlEntryURI) {
     const es = registry.get('entrystore');
     // eslint-disable-next-line arrow-body-style
-    return es.getEntry(etlEntryURI).then((etlEntry) => {
-      return api.load(etlEntry).then((data) => {
+    return es.getEntry(etlEntryURI)
+      .then(etlEntry => api.load(etlEntry))
+      .then((data) => {
         const status = api.status(data);
         if (status !== api.oldStatus(etlEntry)) {
           return api.update(etlEntry, data).then(() => status);
         }
         return status;
       });
-    });
   },
   showFooterResult(message = null) {
     const modalFooter = this.progressDialog.getModalFooter();
