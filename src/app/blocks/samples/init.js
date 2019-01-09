c7dcc779a7bb01efc358844b1b40d12a14f7a541
scazan <scottcazan@gmail.com>
2019-01-09 12:01:36 +0100

    Updating skr

--- src/app/blocks/samples/init.js
+++ src/app/blocks/samples/init.js
@@ -1,22 +1,12 @@
 var debug = document.location.search.indexOf('debug') > 0;
 var path = debug ? '../../' : 'https://static.entryscape.com/';
-var css = function (url) {
-  document.write('<link rel="stylesheet" href="' + path + url + '"/>');
-};
 
 var js = function (url) {
   document.write('<script src="' + path + url + '"/><\/script>');
 };
 
-if (!debug) {
-  document.write('<script src="https://static.entryscape.com/libs/jquery/dist/jquery.min.js" type="text/javascript"><\/script>');
-  document.write('<script src="https://static.entryscape.com/libs/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"><\/script>');
-}
-
 if (debug) {
-  // css('samples/style.css');
   js('app.js');
 } else {
-  css('blocks/0.11/style.css');
-  js('blocks/0.11/all.js');
+  js('blocks/0.13/app.js');
 }
