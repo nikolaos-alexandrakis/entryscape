b64328d0c84f8d9aa96ac65987af370134ac7e3d
scazan <scottcazan@gmail.com>
2019-01-30 16:21:23 +0100

    Get metadata from distribution

--- src/module/catalog/src/datasets/components/Distribution.js
+++ src/module/catalog/src/datasets/components/Distribution.js
@@ -1,16 +1,16 @@
 import m from 'mithril';
+import registry from 'commons/registry';
+import config from 'config';
+import { engine, utils as rdformsUtils } from 'rdforms';
 
-  const renderMetadata = () => {
-    const md = this.entry.getMetadata();
-    const subj = this.entry.getResourceURI();
-    const title = md.findFirstValue(subj, ns.expand('dcterms:title'));
-    const access = md.findFirstValue(subj, ns.expand('dcat:accessURL'));
-    const downloadURI = md.findFirstValue(subj, ns.expand('dcat:downloadURL'));
-    // clear format ,title,urlNode
-    const empty = '';
-    this.formatNode.innerHTML = empty;
-    this.titleNode.innerHTML = empty;
-    this.urlNode.innerHTML = empty;
+export default () => {
+  const namespaces = registry.get('namespaces');
+  const getMetadata = (entry) => {
+    const md = entry.getMetadata();
+    const subj = entry.getResourceURI();
+    const title = md.findFirstValue(subj, namespaces.expand('dcterms:title'));
+    const access = md.findFirstValue(subj, namespaces.expand('dcat:accessURL'));
+    const downloadURI = md.findFirstValue(subj, namespaces.expand('dcat:downloadURL'));
 
     let format;
     // Check for template driven format
@@ -24,46 +24,48 @@ import m from 'mithril';
       const formatPropArr = typeof config.catalog.formatProp === 'string' ? [config.catalog.formatProp] :
         config.catalog.formatProp;
       formatPropArr.find((prop) => {
-        format = md.findFirstValue(subj, ns.expand(prop));
+        format = md.findFirstValue(subj, namespaces.expand(prop));
         return format != null;
       });
     }
     // If there is a nice format, show it
     if (format !== '' && format != null) {
-      this.titleTd.classList.toggle('withFormat', format !== '' && format != null);
-      this.formatNode.innerHTML = format;
+      // this.titleTd.classList.toggle('withFormat', format !== '' && format != null);
+      // this.formatNode.innerHTML = format;
     }
 
     if (title != null && title !== '') {
       // add title
-      this.titleNode.innerHTML = title;
+      // this.titleNode.innerHTML = title;
     } else {
-      this.renderTitle();
+      // this.renderTitle();
     }
     if ((title == null) && (format == null)) {
       // add accessURL or download URL
-      this.urlNode.innerHTML = access || downloadURI;
+      // this.urlNode.innerHTML = access || downloadURI;
     }
-    // this.titleNode, "innerHTML", title || desc || access);
-    // domClass.toggle(this.titleTd, "withFormat", format !== "" && format != null);
-    // domAttr.set(this.formatNode, "innerHTML", format || "");
-    this.modDate = this.entry.getEntryInfo().getModificationDate();
-    this.renderDate();
-    this.clearDropdownMenu();
-    this.renderDropdownMenu();
-  };
+    const modificationDate = entry.getEntryInfo().getModificationDate();
+    // this.renderDate();
+    // this.clearDropdownMenu();
+    // this.renderDropdownMenu();
 
-export default () => {
+    return {
+      title,
+      format,
+      modificationDate,
+    };
+  };
   return {
     view: (vnode) => {
       const { distribution } = vnode.attrs;
 
-      console.log(distribution);
+      const { title, format, modificationDate } = getMetadata(distribution);
+
       return (
         <div tabindex="0" class="distribution__row flex--sb">
           <div class="distribution__format flex--sb">
-            <p class="distribution__title"> Downloadable file</p>
-            <p class="file__format">CSV <span class="file__format--long">Common Separated Values</span></p>
+            <p class="distribution__title">{ title }</p>
+            <p class="file__format">{ format } <span class="file__format--long">Common Separated Values</span></p>
           </div>
           <div class="icon--wrapper">
             <p class="distribution__date">Jan 17</p>
