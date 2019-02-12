b67852b8ea9a2fed7cc6930ef40bf31e97a5e25e
Althea <althea@metasolutions.se>
2019-02-12 16:00:22 +0100

    make the whole card clickable

--- src/module/commons/src/overview/components/StatBox.js
+++ src/module/commons/src/overview/components/StatBox.js
@@ -7,11 +7,9 @@ export default {
   view(vnode) {
     return m('.row', vnode.attrs.bList.map((item) => {
       const { label, value, link } = item;
-      return m('.col-md-6 escoOverview__statBox', [
-        m('a', { href: link }, [
+      return m('a.col-md-6 escoOverview__statBox',{ href: link },[
           m('p', { class: 'escoOverview__statCount' }, value),
-        ]),
-        m('label', { class: 'escoOverview__statLabel' }, label),
+          m('label', { class: 'escoOverview__statLabel' }, label),
       ]);
     }));
   },
