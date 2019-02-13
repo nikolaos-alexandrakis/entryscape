16564ef3f99de5619113e18bf1aa803129ccdf8a
Valentino Hudhra <v.hudhra@gmail.com>
2019-02-13 10:02:37 +0100

    Make head and footer logo consistent

--- src/module/commons/src/nav/components/logo/IconLogo.js
+++ src/module/commons/src/nav/components/logo/IconLogo.js
@@ -4,12 +4,12 @@ export default {
   view(vnode) {
     const { type, src: { icon }, text, isFooter = false } = vnode.attrs;
 
-    const classes = [];
-    if (!isFooter) {
-      classes.push('logo-text', 'icon-text');
-      if (type !== 'icon') { // we are rendering both full and icon logo
-        classes.push('hidden-md', 'hidden-lg hidden-xl');
-      }
+    const classes = ['logo-text', 'icon-text'];
+    if (type !== 'icon') { // we are rendering both full and icon logo
+      classes.push('hidden-md', 'hidden-lg hidden-xl');
+    }
+    if (isFooter) {
+      classes.push('footer-logo-text');
     }
 
     return m('div', { class: classes.join(' ') }, [
