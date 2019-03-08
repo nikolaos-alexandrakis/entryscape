52f9079ff6f794d8f3e1b1ca1666b1f615ff113a
Althea <althea@metasolutions.se>
2019-03-08 11:18:58 +0100

    stats skeleton

--- src/module/catalog/src/statistics/View.js
+++ src/module/catalog/src/statistics/View.js
@@ -1,14 +1,40 @@
 import MithrilView from 'commons/view/MithrilView';
 import declare from 'dojo/_base/declare';
+import './index.scss';
+
 
 export default declare(MithrilView, {
   mainComponent: () => ({
     view(vnode) {
       return (
-        <div>
-          <div className="col-md-12">here you can find some info about stats</div>
-          <div className="col-md-6">here's going to be the left view</div>
-          <div className="col-md-6">here's going to be the right view</div>
+        <div className="">
+          <div className="">
+            <h3>Here you can find some <span>info about stats</span></h3>
+          </div>
+          <section className="stats__wrapper">
+            <div className="data__wrapper">
+              <div className="chooser__wrapper">
+                <h4>Time frame</h4>
+                <div class="chooser">I am a chooser component</div>
+              </div>
+              <div className="distributions__wrapper">
+                <div class="distributionList__tabs">
+                  <p><span class="fa fa-file"></span>Files</p>
+                  <p><span class="fa fa-repeat"></span>API Calls</p>
+                </div>
+                <div class="distributionList"></div>
+              </div>
+              <div className="pagination__wrapper">
+                <div className="pagination">Pagination</div>
+              </div>
+
+            </div>
+            <div className="visualization__wrapper">
+              <h4>Catalog/Distribution statistics for <span>2018</span></h4>
+              <div class="visualization__chart"></div>
+            </div>
+
+          </section>
         </div>
       );
     },
