559f48107a7776edb2d4db5dfe720b36ca50552c
scazan <scottcazan@gmail.com>
2019-03-13 11:07:22 +0100

    Code cleanup, pulling out metadata tools

--- src/module/catalog/src/datasets/components/Overview/index.js
+++ src/module/catalog/src/datasets/components/Overview/index.js
@@ -4,27 +4,30 @@ import registry from 'commons/registry';
 import { i18n } from 'esi18n';
 import dateUtil from 'commons/util/dateUtil';
 import comments from 'commons/comments/comments';
-import StatBox from 'commons/overview/components/StatBox';
-import Toggle from 'commons/components/common/toggle/Toggle';
-import DistributionList from '../DistributionList';
-import MoreMetadata from '../MoreMetadata';
-import Button from '../Button';
 import escaPublicNLS from 'catalog/nls/escaPublic.nls';
 import escaDatasetNLS from 'catalog/nls/escaDataset.nls';
 import escoListNLS from 'commons/nls/escoList.nls';
 import {
   getTitle,
+  getDescription,
   getModifiedDate,
   getThemeLabels,
   getParentCatalogEntry,
+  getContributors,
+  getIdeas,
+  getShowcases,
 } from 'commons/util/metadata';
+import StatBox from 'commons/overview/components/StatBox';
+import Toggle from 'commons/components/common/toggle/Toggle';
+import DistributionList from '../DistributionList';
+import MoreMetadata from '../MoreMetadata';
+import Button from '../Button';
 import bindActions from './actions';
 import './index.scss';
 
 
 export default (vnode) => {
   const { entry } = vnode.attrs;
-  const resourceURI = entry.getResourceURI();
   const entryInfo = entry.getEntryInfo();
 
   const state = {
@@ -48,41 +51,36 @@ export default (vnode) => {
   };
 
   const rdfutils = registry.get('rdfutils');
-
-
   let catalogEntry;
+  const setParentCatalogEntry = () => {
+    getParentCatalogEntry(entry)
+      .then((parentEntry) => {
+        catalogEntry = parentEntry;
+        m.redraw();
+      });
+  };
+
   let contributors;
-  const getContributors = () => {
-    const es = registry.get('entrystore');
-    const contributorsEntryURIs = entryInfo.getContributors()
-      .map(contributorURI => es.getEntryURIFromURI(contributorURI));
-
-    return Promise.all(contributorsEntryURIs.map(uri => es.getEntry(uri)));
+  const setContributors = () => {
+    getContributors(entry).then((entries) => {
+      contributors = entries;
+      m.redraw();
+    });
   };
 
-  const getSolrQueryResults = (entry, type, onSuccess) => {
-    const context = registry.get('context');
-    const es = registry.get('entrystore');
-    const ns = registry.get('namespaces');
-
-    es.newSolrQuery().rdfType(ns.expand(type))
-      .uriProperty('dcterms:source', entry.getResourceURI())
-      .context(context.getResourceURI())
-      .getEntries()
-      .then(onSuccess);
-  };
 
   let ideas = [];
-  const getIdeas = (entry) => {
-    getSolrQueryResults(entry, 'esterms:Idea', (entries) => {
+  const setIdeas = () => {
+    getIdeas(entry, (entries) => {
       ideas = entries;
       m.redraw();
     });
   };
 
+
   let showcases = [];
-  const getShowcases = (entry) => {
-    getSolrQueryResults(entry, 'esterms:Result', (entries) => {
+  const setShowcases = () => {
+    getShowcases(entry, (entries) => {
       showcases = entries;
       m.redraw();
     });
@@ -107,34 +105,24 @@ export default (vnode) => {
     oninit() {
       // Cache the entry context
       entry.getContext().getEntry();
-      getParentCatalogEntry(entry)
-        .then((parentEntry) => {
-          catalogEntry = parentEntry;
-          m.redraw();
-        });
-      getContributors().then((entries) => {
-        contributors = entries;
-        m.redraw();
-      });
+      setParentCatalogEntry()
+      setContributors();
       refreshComments();
-      getIdeas(entry);
-      getShowcases(entry);
+      setIdeas();
+      setShowcases();
     },
     view: () => {
-      const metadata = entry.getMetadata();
-
-      const title = metadata.findFirstValue(resourceURI, 'dcterms:title');
-      const lastUpdatedDate = dateUtil.getMultipleDateFormats(getModifiedDate(entry));
-      const description = metadata.findFirstValue(resourceURI, 'dcterms:description');
-      const themes = getThemeLabels(entry);
-
       const escaDataset = i18n.getLocalization(escaDatasetNLS);
       const escaPublic = i18n.getLocalization(escaPublicNLS);
       const escoList = i18n.getLocalization(escoListNLS);
-      // const publishToggleString = state.isPublished ? escaDataset.publishedTitle : escaDataset.unpublishedTitle;
+
+      const title = getTitle(entry);
+      const lastUpdatedDate = dateUtil.getMultipleDateFormats(getModifiedDate(entry));
+      const description = getDescription(entry);
+      const numRevisions = entry.getEntryInfo().getMetadataRevisions().length;
+      const themes = getThemeLabels(entry);
       const isPublished = entry.isPublic();
       const publishToggleString = isPublished ? escaDataset.publishedTitle : escaDataset.unpublishedTitle;
-
       const catalogName = catalogEntry ? rdfutils.getLabel(catalogEntry) : null;
       const contributorsNames = contributors ? contributors.map(contributor => rdfutils.getLabel(contributor)) : null;
 
@@ -147,24 +135,24 @@ export default (vnode) => {
                 <p class="description">{ description }</p>
               </div>
               <div class="metadata--basic">
-                { catalogName && 
+                { catalogName && (
                   <p><span class="metadata__label">
                     {escaPublic.datasetBelongsToCatalog}: </span> {catalogName}
                   </p>
-                }
-                { themes.length > 0 &&
-                    <p>
-                      <span class="metadata__label">{escaDataset.themeTitle}: </span>{themes.join(', ')}
-                    </p>
-                }
+                )}
+                { themes.length > 0 && (
+                  <p>
+                    <span class="metadata__label">{escaDataset.themeTitle}: </span>{themes.join(', ')}
+                  </p>
+                )}
 
                 <p><span class="metadata__label">{escaDataset.lastUpdateLabel}: </span> {lastUpdatedDate.short}</p>
-                { contributorsNames &&
+                { contributorsNames && (
                   <p>
                     <span class="metadata__label">{escaDataset.editedTitle}: </span>
                     {contributorsNames.join(', ')}
                   </p>
-                }
+                )}
                 <Button class=" btn-sm btn-secondary" onclick={toggleMetadata}>{escaDataset.showMoreTitle}</Button>
 
               </div>
@@ -206,7 +194,7 @@ export default (vnode) => {
           <div class="flex--sb">
             <DistributionList dataset={entry}></DistributionList>
             <div class="cards--wrapper">
-              <StatBox value="3" label={escoList.versionsLabel} onclick={actions.openRevisions}/>
+              <StatBox value={numRevisions} label={escoList.versionsLabel} onclick={actions.openRevisions}/>
               <StatBox value={numComments} label={escaDataset.commentMenu} onclick={openCommentsDialog}/>
               <StatBox value={ideas.length} label={escaDataset.showideas} onclick={actions.openIdeas}/>
               <StatBox value={showcases.length} label={escaDataset.showresults} onclick={actions.openShowcases}/>
--- src/module/commons/src/util/metadata.js
+++ src/module/commons/src/util/metadata.js
@@ -9,6 +9,17 @@ const getChoiceLabels = (entry, property, choiceTemplateId) => {
     .map(uri => choices.find(choice => choice.value === uri) )
     .map(choice => registry.get('localize')(choice.label));
 };
+const getSolrQueryResults = (entry, type, onSuccess) => {
+  const context = registry.get('context');
+  const es = registry.get('entrystore');
+  const ns = registry.get('namespaces');
+
+  es.newSolrQuery().rdfType(ns.expand(type))
+    .uriProperty('dcterms:source', entry.getResourceURI())
+    .context(context.getResourceURI())
+    .getEntries()
+    .then(onSuccess);
+};
 // END UTILS
 
 export const getTitle = (entry) => {
@@ -22,9 +33,31 @@ export const getTitle = (entry) => {
 
   return name;
 };
+export const getDescription = (entry) => {
+  const metadata = entry.getMetadata();
+  const resourceURI = entry.getResourceURI();
+  return metadata.findFirstValue(resourceURI, 'dcterms:description');
+};
 
 export const getModifiedDate = entry => entry.getEntryInfo().getModificationDate();
 export const getThemeLabels = entry => getChoiceLabels(entry, 'dcat:theme', 'dcat:theme-isa');
 
 export const getParentCatalogEntry = entry => registry.get('entrystoreutil')
   .getEntryByType('dcat:Catalog', entry.getContext());
+
+export const getContributors = (entry) => {
+  const es = registry.get('entrystore');
+  const entryInfo = entry.getEntryInfo();
+  const contributorsEntryURIs = entryInfo.getContributors()
+    .map(contributorURI => es.getEntryURIFromURI(contributorURI));
+
+  return Promise.all(contributorsEntryURIs.map(uri => es.getEntry(uri)));
+};
+
+export const getIdeas = (entry, onSuccess) => {
+  getSolrQueryResults(entry, 'esterms:Idea', onSuccess);
+};
+
+export const getShowcases = (entry, onSuccess) => {
+  getSolrQueryResults(entry, 'esterms:Result', onSuccess);
+};
