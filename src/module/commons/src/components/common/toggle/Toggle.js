07050c96001c6386b25601dd8b9588ef7303cddf
Althea <althea@metasolutions.se>
2019-03-14 10:38:43 +0100

    Add tooltip to publish toggle

--- src/module/commons/src/components/common/toggle/Toggle.js
+++ src/module/commons/src/components/common/toggle/Toggle.js
@@ -2,11 +2,11 @@ import m from 'mithril';
 
 export default () => ({
   view(vnode) {
-    const { isEnabled, onToggle } = vnode.attrs;
+    const { isEnabled, onToggle, title } = vnode.attrs;
     const enabledClass = isEnabled ? '' : 'fa-rotate-180';
 
     return (
-      <button class={`fa fa-toggle-on fa-lg  btn--publish ${enabledClass}`} onclick={onToggle} ></button>
+      <button class={`fa fa-toggle-on fa-lg  btn--publish ${enabledClass}`} title={title} onclick={onToggle} ></button>
     );
   },
 });
