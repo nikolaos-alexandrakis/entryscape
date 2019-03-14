1f7a9d3151d827a17256b1e4f8d20c7b59a00a87
Althea <althea@metasolutions.se>
2019-03-14 17:20:01 +0100

    Make dropdown go up when there is no enough space and fix extra footer space

--- src/module/commons/src/components/common/Dropdown/index.js
+++ src/module/commons/src/components/common/Dropdown/index.js
@@ -1,4 +1,5 @@
 import m from 'mithril';
+import jquery from 'jquery';
 import { createSetState } from 'commons/util/util';
 import './index.scss';
 
@@ -13,6 +14,7 @@ export default (vnode) => {
     setState({
       isShowing: false,
     });
+    vnode.dom.classList.remove('dropup');
   };
 
   const handleOutsideClick = (e) => {
@@ -33,6 +35,23 @@ export default (vnode) => {
     });
   };
 
+
+  const setDropdownOrientation = () => {
+    const dropdownElement = vnode.dom.querySelector('.row__dropdownMenu');
+    const dropdownHeight = dropdownElement.offsetHeight;
+    const toggleButton = vnode.dom.querySelector('.dropdown-toggle');
+    const toggleButtonHeight = toggleButton.offsetHeight;
+    const dropdownScreenPositionY = toggleButton.getBoundingClientRect().top;
+
+    const spaceAbove = dropdownScreenPositionY;
+    const spaceBelow = Math.abs(window.innerHeight - dropdownScreenPositionY);
+    const elementHeight = toggleButtonHeight + dropdownHeight;
+
+    if ((spaceBelow < elementHeight) && (spaceAbove > spaceBelow) ) {
+      dropdownElement.classList.add('dropup');
+    }
+  };
+
   return {
     view(vnode) {
       const { children } = vnode;
@@ -40,12 +59,17 @@ export default (vnode) => {
 
       return (
         <div>
-          <button class="icons fa fa-cog" onclick={toggleDropdown}></button>
-          <div class={`file__dropdownMenu ${showingDropdownClass}`}>
+          <button class="icons fa fa-cog dropdown-toggle" onclick={toggleDropdown}></button>
+          <div class={`row__dropdownMenu ${showingDropdownClass}`}>
             { children }
           </div>
         </div>
       );
+    },
+    onupdate() {
+      if (state.isShowing) {
+        setDropdownOrientation();
+      }
     },
   };
 };
