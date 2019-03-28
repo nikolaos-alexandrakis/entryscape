d70cc4b30b28fcfc491472e9462e9995dca818ff
Valentino Hudhra <v.hudhra@gmail.com>
2019-03-28 11:04:34 +0100

    Add __temporary context entry in the cache to avoid a Lookup failure for registry uploads

--- src/app/registry/src/present/Dataset.js
+++ src/app/registry/src/present/Dataset.js
@@ -9,6 +9,20 @@ export default declare([Dataset], {
   postCreate() {
     this.inherited('postCreate', arguments);
     this.distributionInfoDialog.checkForAPI = false;
+
+    /**
+     * @todo improve explanation
+     * Because the entry/context template information is lost at this point given that we harvested the data
+     * we fake  the existence of a contextEntry entry "__temporary" (by putting it in the es.js cache)
+     * in order for the Lookup.template not to fail
+     */
+    const es = registry.getEntryStore();
+    const cache = es.getCache();
+    let __temporaryContextEntry = cache.get(es.getEntryURI('_contexts', '__temporary'));
+    if (__temporaryContextEntry == null) {
+      __temporaryContextEntry = es.newContext('__temporary', '__temporary');
+      cache.cache(__temporaryContextEntry, true);
+    }
   },
   /**
    *
