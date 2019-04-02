32db24adf5c25ca583858f2fe063a731470d562c
Valentino Hudhra <v.hudhra@gmail.com>
2019-04-02 12:14:12 +0200

    First round for pagination component

---
+++ src/module/commons/src/components/common/Pagination/index.js
@@ -0,0 +1,38 @@
+import m from 'mithril';
+import './index.scss';
+
+// @todo @valentino check alternative to passing state
+const onChangePage = (state, callback, newPage) => {
+  // call callback
+  callback(newPage);
+
+  state.currentPage = newPage;
+  m.redraw();
+};
+
+export default () => ({
+  view(vnode) {
+    const { pageSize = 20, totalCount, handleChangePage } = vnode.attrs;
+    // calculate (from - to of total)
+    const currentPage = vnode.state.currentPage || 0;
+    const fromCount = !currentPage ? 0 : currentPage * pageSize;
+    const toCount = (fromCount + pageSize) < totalCount ? fromCount + pageSize : totalCount;
+
+    // @todo perhaps improve this if needed
+    const onChangePagePrevious = onChangePage.bind(null, vnode.state, handleChangePage, currentPage - 1);
+    const onChangePageNext = onChangePage.bind(null, vnode.state, handleChangePage, currentPage + 1);
+
+    return (<ul className="pagination">
+      <li>
+        <button disabled={!currentPage} className={!currentPage ? 'disabled' : ''} onclick={onChangePagePrevious}>
+          <i className="fa fa-chevron-left"/></button>
+      </li>
+      <span>{fromCount}-{toCount} of {totalCount}</span>
+      <li>
+        <button disabled={toCount === totalCount} className={toCount === totalCount ? 'disabled' : ''}
+                onclick={onChangePageNext}><i
+          className="fa fa-chevron-right"/></button>
+      </li>
+    </ul>);
+  },
+});
