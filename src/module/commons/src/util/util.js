8c40df10557dbef030d4593cd36f0c0320c175ad
Valentino Hudhra <v.hudhra@gmail.com>
2019-04-08 15:25:27 +0200

    Use lodash merge instead of Object.assign shallow merge

--- src/module/commons/src/util/util.js
+++ src/module/commons/src/util/util.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import isUrl from 'is-url';
+import { merge } from 'lodash-es';
 import m from 'mithril';
 
 export const isUri = stringToCheck => isUrl(stringToCheck);
@@ -12,7 +13,7 @@ export const isUri = stringToCheck => isUrl(stringToCheck);
  * @returns {function}
  */
 export const createSetState = state => (props, avoidRedraw = false) => {
-  Object.assign(state, props);
+  merge(state, props);
   if (!avoidRedraw) {
     m.redraw();
   }
