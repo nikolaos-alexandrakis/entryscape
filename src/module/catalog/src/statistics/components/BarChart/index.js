b531ecc5618097cf2592f3181bc2cb190a4a9aee
Valentino Hudhra <v.hudhra@gmail.com>
2019-04-11 16:51:18 +0200

    Remove chartist.js import in statistics

--- src/module/catalog/src/statistics/components/BarChart/index.js
+++ src/module/catalog/src/statistics/components/BarChart/index.js
@@ -1,58 +1,12 @@
 import escaStatistics from 'catalog/nls/escaStatistics.nls';
 import Chart from 'chart.js';
-// import Chartist from 'chartist';
 import 'chartist-plugin-legend';
 import 'chartist-plugin-tooltips';
 import 'chartist-plugin-tooltips/dist/chartist-plugin-tooltip.css';
 import { i18n } from 'esi18n';
-// import moment from 'moment'; // @todo valentino
 import './index.scss';
 
 let chart; // @todo @valentino
-
-const getChartOptions = (xAxisDateFormat = 'MMM D') => ({
-  showLine: false,
-  axisX: {
-    offset: 20,
-    type: Chartist.FixedScaleAxis,
-    divisor: 5,
-    ticks: [
-      new Date(2019, 0),
-      new Date(2019, 1),
-      new Date(2019, 2),
-      new Date(2019, 3),
-      new Date(2019, 4),
-      new Date(2019, 5),
-      new Date(2019, 6),
-      new Date(2019, 7),
-      new Date(2019, 8),
-      new Date(2019, 9),
-      new Date(2019, 10),
-      new Date(2019, 11),
-    ],
-    labelInterpolationFnc(value) {
-      console.log(value);
-      return moment(value).format(xAxisDateFormat);
-      // return moment(value);
-    },
-  },
-  axisY: {
-    type: Chartist.AutoScaleAxis,
-    onlyInteger: true,
-    // offset: 10,
-  },
-  // low: 0,
-  plugins: [
-    Chartist.plugins.tooltip({
-      appendToBody: true,
-      transformTooltipTextFnc: (value) => {
-        const [timestamp, downloadCount] = value.split(',');
-        const date = moment(parseInt(timestamp, 10));
-        return `${downloadCount} downloads on ${date.format('ll')}`;
-      },
-    }),
-  ],
-});
 
 /**
  * Given a length guess what date format is appropriate to render.
