57312bc0e4bb811d40ab00692801ec8b01e7e0d5
Althea <althea@metasolutions.se>
2019-04-17 11:09:35 +0200

    Replace dl-horizontal(dropped)

--- src/module/commons/src/components/common/keyvalue/KeyValueList.js
+++ src/module/commons/src/components/common/keyvalue/KeyValueList.js
@@ -11,7 +11,7 @@ export default {
   view(vnode) {
     const { data, dtClass = '', ddClass = '', asBadge = false } = vnode.attrs;
 
-    return m('dl.dl-horizontal',
+    return m('dl.dl--horizontal',
       Object.keys(data).map(key => m('div', { key: key.replace(/\s/g, '') }, [
         m('dt', { class: dtClass }, key),
         m('dd', { class: ddClass }, asBadge ? m('span.badge', data[key]) : data[key]),
