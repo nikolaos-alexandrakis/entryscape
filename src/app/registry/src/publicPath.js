356a1b3a1c80de1684c202673aa06b3c352b4151
Valentino Hudhra <v.hudhra@gmail.com>
2019-04-24 10:33:43 +0200

    Add a debug flag to the local.js to avoid  requesting anything from static

--- src/app/registry/src/publicPath.js
+++ src/app/registry/src/publicPath.js
@@ -1,8 +1,13 @@
 /**
+/**
  * NOTE! this is needed to set the webpack public path on the fly. Do NOT remove.
  * @see https://webpack.js.org/guides/public-path/
  */
 
-const { localBuild = false } = __entryscape_config.entryscape;
+const { localBuild = false, debug = false } = __entryscape_config.entryscape;
 const { url, app, version } = __entryscape_config.entryscape.static;
-__webpack_public_path__ = localBuild ? '/dist/' : `${url.endsWith('/') ? url : `${url}/`}${app}/${version}/`; // eslint-disable-line
+if (debug) {
+  __webpack_public_path__ = '/'; // eslint-disable-line
+} else {
+  __webpack_public_path__ = localBuild ? '/dist/' : `${url.endsWith('/') ? url : `${url}/`}${app}/${version}/`; // eslint-disable-line
+}
