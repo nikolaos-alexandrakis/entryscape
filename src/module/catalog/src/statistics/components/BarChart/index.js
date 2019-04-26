dcb1be5c4a3718715263a5bfafa60c3dc3982e29
Althea <althea@metasolutions.se>
2019-04-26 16:14:14 +0200

    Add colour palette

--- src/module/catalog/src/statistics/components/BarChart/index.js
+++ src/module/catalog/src/statistics/components/BarChart/index.js
@@ -29,13 +29,28 @@ const guessAxisFormatFromData = (dataLength) => {
 
 const COLOR_OPTIONS = [
   {
-    borderColor: '#165b98',
-    backgroundColor: 'rgba(22, 91, 152,0.2)',
+    borderColor: '#00838f',
+    backgroundColor: 'rgba(0, 131, 143,0.2)',
     borderWidth: 3,
   },
   {
     borderColor: '#165b98',
     backgroundColor: 'rgba(22, 91, 152,0.2)',
+    borderWidth: 3,
+  },
+  {
+    borderColor: '#aed581',
+    backgroundColor: 'rgba(174, 213, 129,0.2)',
+    borderWidth: 3,
+  },
+  {
+    borderColor: '#fbc02d',
+    backgroundColor: 'rgba(251, 192, 45,0.2)',
+    borderWidth: 3,
+  },
+  {
+    borderColor: '#e91e63',
+    backgroundColor: 'rgba(233, 30, 99,0.2)',
     borderWidth: 3,
   },
 ];
