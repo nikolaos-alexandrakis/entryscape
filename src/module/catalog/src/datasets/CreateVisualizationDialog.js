f714cb018d98cffb255e9e466288fa3b60cc1b05
scazan <scottcazan@gmail.com>
2019-05-02 17:06:12 +0200

    Only show operations when you are not on Map

--- src/module/catalog/src/datasets/CreateVisualizationDialog.js
+++ src/module/catalog/src/datasets/CreateVisualizationDialog.js
@@ -177,6 +177,7 @@ const getControllerComponent = (datasetEntry, files) => {
               y={state.yAxisField}
               operation={state.operation}
               data={csvData}
+              type={state.chartType}
               onSelect={onAxisUpdate}
             />
           </div>
