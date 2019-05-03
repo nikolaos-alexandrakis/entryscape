0480bb4d062b35cf14d211133081adac73cf5869
Althea <althea@metasolutions.se>
2019-05-03 11:56:22 +0200

    Add fontawesome icons to type graph

--- src/module/catalog/src/visualization/components/TypeSelector/index.js
+++ src/module/catalog/src/visualization/components/TypeSelector/index.js
@@ -19,28 +19,32 @@ export default (vnode) => {
       const { type } = vnode.attrs;
 
       return (
-          <div class="graphType__card__wrapper">
-            <div class="btn-group btn-group-toggle" data-toggle="buttons">
-              <label class="btn btn-secondary btn-raised active">
-                <input type="radio" name="graphType"
-                  onchange={selectMap}
-                ></input>
-                Map
-              </label>
-              <label class="btn btn-secondary btn-raised">
-                <input type="radio" name="graphType"
-                  onchange={selectBar}
-                ></input>
+        <div class="graphType__card__wrapper">
+          <div class="btn-group btn-group-toggle" data-toggle="buttons">
+            <label class="btn btn-secondary btn-raised graphCard active">
+              <input type="radio" name="graphType"
+                onchange={selectMap}
+              ></input>
+              <span class="fas fa-map-marked"></span>
+               Map
+            </label>
+            <label class="btn btn-secondary btn-raised graphCard">
+              <input type="radio" name="graphType"
+                onchange={selectBar}
+              ></input>
+              <span class="fas fa-chart-bar"></span>
                 Bar Chart
-              </label>
-              <label class="btn btn-secondary btn-raised">
-                <input type="radio" name="graphType"
-                  onchange={selectLine} checked="checked" data-blah="heya"
-                ></input>
+            </label>
+            <label class="btn btn-secondary btn-raised graphCard">
+              <input type="radio" name="graphType"
+                onchange={selectLine} checked="checked" data-blah="heya"
+              ></input>
+                 <span class="fas fa-chart-line"></span>
+
                 Line Chart
-              </label>
-            </div>
+            </label>
           </div>
+        </div>
       );
     },
   };
