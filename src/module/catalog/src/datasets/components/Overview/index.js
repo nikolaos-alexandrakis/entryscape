9e1016adeb3e6a9d4c2508e5b1d74179e73e50c0
Althea <althea@metasolutions.se>
2019-05-06 09:44:47 +0200

    Fix btn css and remove blue background on catalog stats

--- src/module/catalog/src/datasets/components/Overview/index.js
+++ src/module/catalog/src/datasets/components/Overview/index.js
@@ -298,6 +298,7 @@ export default (vnode) => {
 
           <div class="flex--sb">
             <DistributionList dataset={entry}></DistributionList>
+            <div class="charts__container">
             {visualizationEntryConfigurations
               .map(configurationEntry =>
                 <VisualizationPreview
@@ -310,6 +311,8 @@ export default (vnode) => {
                   }
                 />)
             }
+            </div>
+            
           </div>
         </main>
       );
