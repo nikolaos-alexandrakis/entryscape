c15dd45c8c1e59980d5b09a2630cfdc1335934bc
Valentino Hudhra <v.hudhra@gmail.com>
2019-05-13 16:48:53 +0200

    Move stats dialog chart in a separate file

--- src/module/catalog/src/datasets/StatisticsDialog.js
+++ src/module/catalog/src/datasets/StatisticsDialog.js
@@ -1,48 +1,7 @@
+import DistributionChart from 'catalog/datasets/components/DistributionChart';
 import escaStatistics from 'catalog/nls/escaStatistics.nls';
-import { getMultiDatasetChartData } from 'catalog/statistics/utils/chart';
-import timeRangeUtil from 'catalog/statistics/utils/timeRange';
-import Chart from 'commons/components/common/chart/TimeBarChart';
-import SearchSelect from 'commons/components/common/select/SearchSelect';
 import TitleDialog from 'commons/dialog/TitleDialog';
-import registry from 'commons/registry';
-import { createSetState } from 'commons/util/util';
 import declare from 'dojo/_base/declare';
-import { i18n } from 'esi18n';
-
-const timeRangesItems = timeRangeUtil.getTimeRanges();
-
-const getControllerComponent = (entries) => {
-  const state = {
-    data: [],
-  };
-  const setState = createSetState(state);
-  const defaultTimeRange = 'this-month';
-
-  const loadData = (timeRange) => {
-    const range = typeof timeRange === 'string' ? timeRange : defaultTimeRange;
-    const context = registry.getContext();
-
-    getMultiDatasetChartData(entries, context, range).then(data => setState({ data }));
-  };
-
-  return {
-    oninit: loadData,
-    view() {
-      const escaStatisticsNLS = i18n.getLocalization(escaStatistics);
-      return <section>
-        <div className="chooser__wrapper">
-          <h4>{escaStatisticsNLS.statsDialogTimeRange}</h4>
-          <SearchSelect
-            options={timeRangesItems}
-            selectedOptions={[defaultTimeRange]}
-            onChange={loadData}
-          />
-        </div>
-        <Chart data={state.data}/>
-      </section>;
-    },
-  };
-};
 
 export default declare([TitleDialog.ContentComponent], {
   nlsBundles: [{ escaStatistics }],
@@ -56,7 +15,8 @@ export default declare([TitleDialog.ContentComponent], {
   },
   open(params) {
     this.dialog.show();
-    const controllerComponent = getControllerComponent(params.entries);
+    const { entries } = params;
+    const controllerComponent = { view: () => m(DistributionChart, { entries }) };
     this.show(controllerComponent);
   },
 });
