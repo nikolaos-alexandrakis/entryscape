52b12c4bfed468b7c89cb377140a5736448e2833
Althea <althea@metasolutions.se>
2019-05-23 10:29:32 +0200

    Remove -xs from col and change some hidden-xs for d-none

--- src/app/registry/src/nav/components/CardComponent.js
+++ src/app/registry/src/nav/components/CardComponent.js
@@ -10,8 +10,8 @@ export default {
       onclick,
     } = vnode.attrs;
 
-    return m('div.col-xs-12.col-md-6', { style: 'padding: 0 7.5px' }, [
-      m(`.col-xs-12 ${id}__card card`, { onclick }, [
+    return m('div.col-12.col-md-6', { style: 'padding: 0 7.5px' }, [
+      m(`.col-12 ${id}__card card`, { onclick }, [
         m('i', { class: `${id}__cardIcon float-right fas fa-2x fa-${faClass}` }, null),
         m('h3', { class: `${id}__cardHeader` }, title),
         m('p', { class: `${id}__cardParagraph` }, text),
