42279c29b2457cc50db580be5e3cd0a171f61efa
Matthias Palmer <matthias@metasolutions.se>
2019-08-23 16:13:47 +0200

    Chart test

---
+++ src/app/blocks/samples/graphics/chart.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+/* eslint-disable no-template-curly-in-string */
+window.__entryscape_config = {
+  block: 'config',
+  page_language: 'sv',
+  entrystore: 'https://opendata.statenskonstrad.se/store',
+  blocks: [
+    {
+      proportion: 'ct-octave',
+      url: "https://registrera.oppnadata.se/charts/orgData.json",
+      block: 'chartExample',
+      extends: 'chart',
+      type: 'bar',
+      label: 'scb3_text',
+      limit: 15,
+    },
+  ],
+};
