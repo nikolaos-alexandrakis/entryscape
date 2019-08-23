705d18d525e32ebb844af0699ea9be2250d37de8
Valentino Hudhra <v.hudhra@gmail.com>
2019-08-23 11:52:59 +0200

    Make localBuild work

--- src/app/suite/src/config/config.js
+++ src/app/suite/src/config/config.js
@@ -15,7 +15,12 @@ const STATIC = {
   APP: 'suite',
   VERSION: 'latest',
 };
-const ASSETS_URL = __entryscape_config.entryscape.localBuild ? '/dist/assets/' : `${STATIC.URL}${STATIC.APP}/${STATIC.VERSION}/assets/`;
+
+/**
+ * @see publicPath.js
+ * @type {string}
+ */
+const ASSETS_URL = __entryscape_config.entryscape.localBuild ? `${__webpack_public_path__}/assets/` : `${STATIC.URL}${STATIC.APP}/${STATIC.VERSION}/assets/`;
 const LOGO_SVG_URL = `${ASSETS_URL}entryscape.svg`;
 
 const config = merge(adminConfig, catalogConfig, termsConfig, workbenchConfig, {
